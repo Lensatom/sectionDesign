c7244e231cdadfeb22db95d006ca5ea6eaed5559
Ayomide Atayero <77001715+Lensatom@users.noreply.github.com>
2023-07-28 22:49:53 +0100

    Update sectionDesign.tsx

--- sectionDesign.tsx
+++ sectionDesign.tsx
@@ -12,8 +12,8 @@ const sectionDesign = () => {
   }
 
   return (
-    <div style={gradient} className='shadow-lg flex w-[fit-content] bg-red-200 rounded-l-full rounded-r-lg overflow-hidden'>
-      <div style={style} className='w-44 h-44 !bg-primary rounded-full' />
+    <div style={gradient} className='shadow-lg flex w-[fit-content] rounded-l-full rounded-r-lg overflow-hidden'>
+      <div style={style} className='w-44 h-44 rounded-full' />
       <div className='py-4 pl-5'>
         <h2 className='test-gray-900 text-lg font-semibold tracking-wide'>Fast Payments Internationally</h2>
         <p className='text-sm text-gray-600 mr-10 leading-tight'>
